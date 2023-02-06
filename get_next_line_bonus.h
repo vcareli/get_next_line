594780da714be869db0b3eeb1c203206f9772f94
vcareli <vcareli@gmail.com>
2023-02-06 12:08:36 -0500

    9

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,29 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vinvieir <vinvieir@student.42quebec.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/28 12:44:43 by vinvieir          #+#    #+#             */
-/*   Updated: 2023/01/30 14:13:35 by vinvieir         ###   ########.fr       */
+/*   Created: 2023/02/06 12:44:43 by vinvieir          #+#    #+#             */
+/*   Updated: 2023/02/06 14:13:35 by vinvieir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
-# include <unistd.h>
-# include <stdlib.h>
 # include <stddef.h>
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(const char *s1, const char *s2);
 size_t	ft_strlen(const char *s);
 
 #endif
